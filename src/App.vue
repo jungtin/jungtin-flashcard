b6b163cfc14075a3f15e04903c4e620667deb4e8
jungtin_fpt <thinhntse140344@fpt.edu.vn>
2020-11-21 21:06:12 +0700

    COMMIT : UPDATE INIT

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>Xin chào</h1>
+  <NavbarCmp @changeCurrentTab="currentTab = $event"/>
+  <div class="container">
+    <div class="row mt-5">
+      <div class="col">
+        <component :is="currentTab"></component>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import NavbarCmp from '@/components/NavbarCmp.vue';
+import AppHome from '@/components/AppHome.vue';
 export default {
-
+  components: {
+    NavbarCmp,
+    AppHome,
+  },
+  data() {
+    return {
+      currentTab: "AppHome"
+    }
+  },
 }
 </script>
 
